7069676284c7e9f0977fc55bd91cd5872f2fd609
Chinmay Dalal <chinmay.dalal.22012001@gmail.com>
2020-10-21 23:49:52 +0530

    Update

--- nvim/.config/nvim/lua/packages.lua
+++ nvim/.config/nvim/lua/packages.lua
@@ -1,84 +1,94 @@
 local packages = {
-  {'wbthomason/packer.nvim', opt=true},
-  {'Akin909/nvim-bufferline.lua', config = 'require [[bufferline]].setup()'},
-  {'camspiers/lens.vim', requires = {'camspiers/animate.vim'}},
-  {'dense-analysis/ale'},
-  {'dracula/vim', as = 'dracula'},
-  {'editorconfig/editorconfig-vim'},
-  {'guns/vim-sexp',
-   ft = {'clojure', 'fennel'},
-   requires = {'tpope/vim-sexp-mappings-for-regular-people'}
+  {"wbthomason/packer.nvim", opt = true},
+  {"Akin909/nvim-bufferline.lua", config = "require [[bufferline]].setup()"},
+  {"camspiers/lens.vim", requires = {"camspiers/animate.vim"}},
+  {"dense-analysis/ale"},
+  {"dracula/vim", as = "dracula"},
+  {"editorconfig/editorconfig-vim"},
+  {
+    "guns/vim-sexp",
+    ft = {"clojure", "fennel"},
+    requires = {"tpope/vim-sexp-mappings-for-regular-people"}
   },
-  {'hardcoreplayers/dashboard-nvim'},
-  {'jeffkreeftmeijer/vim-numbertoggle'},
-  {'jiangmiao/auto-pairs'},
-  {'justinmk/vim-gtfo'},
-  {'justinmk/vim-sneak'},
-  {'kyazdani42/nvim-tree.lua', requires = 'kyazdani42/nvim-web-devicons'},
-  {'liuchengxu/vista.vim'},
-  {'liuchengxu/vim-which-key'},
-  {'mattn/vim-sonictemplate', cmd = 'Template'},
-  {'nvim-lua/completion-nvim',
-    requires= {
-      {'aca/completion-tabnine', run = './install.sh'},
-      {'ncm2/float-preview.nvim', cond = function() return
-                                          vim.g.gnvim ~=1 and vim.g.uivonim ~= 1
-                                         end
+  {"hardcoreplayers/dashboard-nvim"},
+  {"jeffkreeftmeijer/vim-numbertoggle"},
+  {"jiangmiao/auto-pairs"},
+  {"justinmk/vim-gtfo"},
+  {"justinmk/vim-sneak"},
+  {"kyazdani42/nvim-tree.lua", requires = "kyazdani42/nvim-web-devicons"},
+  {"liuchengxu/vista.vim"},
+  {"liuchengxu/vim-which-key"},
+  {"mattn/vim-sonictemplate", cmd = "Template"},
+  {
+    "nvim-lua/completion-nvim",
+    requires = {
+      {"aca/completion-tabnine", run = "./install.sh"},
+      {
+        "ncm2/float-preview.nvim",
+        cond = function()
+          return vim.g.gnvim ~= 1 and vim.g.uivonim ~= 1
+        end
       },
-      'hrsh7th/vim-vsnip',
-      'hrsh7th/vim-vsnip-integ'
+      "hrsh7th/vim-vsnip",
+      "hrsh7th/vim-vsnip-integ"
     }
   },
-  {'neovim/nvim-lspconfig',
+  {
+    "neovim/nvim-lspconfig",
     requires = {
-      'nvim-lua/lsp-status.nvim',
-      'nvim-lua/diagnostic-nvim'
+      "nvim-lua/lsp-status.nvim",
+      "nvim-lua/diagnostic-nvim"
     },
-    config = {'require [[ls_setup]]'}
+    config = {"require [[ls_setup]]"}
   },
-  {'mfussenegger/nvim-jdtls', opt = true},
-  {'norcalli/nvim-colorizer.lua', config = {'require [[colorizer]].setup()'}},
-  {'nvim-treesitter/nvim-treesitter',
-    config = 'require [[treesitter_setup]]',
+  {"mfussenegger/nvim-jdtls", opt = true},
+  {"norcalli/nvim-colorizer.lua", config = {"require [[colorizer]].setup()"}},
+  {
+    "nvim-treesitter/nvim-treesitter",
+    config = "require [[treesitter_setup]]",
     requires = {
-    'nvim-treesitter/nvim-treesitter-refactor',
-    'nvim-treesitter/nvim-treesitter-textobjects'
+      "nvim-treesitter/nvim-treesitter-refactor",
+      "nvim-treesitter/nvim-treesitter-textobjects"
     }
   },
-  {'nvim-treesitter/playground'},
---  {'romgrk/nvim-treesitter-context'},
-  {'Olical/aniseed', config=[[require('aniseed.env').init({ module = 'dotfiles.init' })]], tag = 'v3.9.0'},
-  {'Olical/conjure', ft = {'fennel', 'clojure'}, tag = 'v4.7.0'},
-  {'pbrisbin/vim-mkdir'},
-  {'psliwka/vim-smoothie'},
-  {'tpope/vim-fugitive'},
-  {'tpope/vim-surround'},
-  {'tyru/caw.vim'},
-  {'reedes/vim-pencil', ft = {'txt', 'md', 'mkdn', 'rst'}},
-  {'rhysd/git-messenger.vim'},
-  {'vigoux/LanguageTool.nvim',
---TODO    ft = {},
+  {"nvim-treesitter/playground"},
+  --  {'romgrk/nvim-treesitter-context'},
+  {"Olical/aniseed", config = [[require('aniseed.env').init({ module = 'dotfiles.init' })]], tag = "v3.9.0"},
+  {"Olical/conjure", ft = {"fennel", "clojure"}, tag = "v4.7.0"},
+  {"pbrisbin/vim-mkdir"},
+  {"psliwka/vim-smoothie"},
+  {"tpope/vim-fugitive"},
+  {"tpope/vim-surround"},
+  {"tyru/caw.vim"},
+  {"reedes/vim-pencil", ft = {"txt", "md", "mkdn", "rst"}},
+  {"rhysd/git-messenger.vim"},
+  {
+    "vigoux/LanguageTool.nvim"
+    --TODO    ft = {},
   },
-  {'Yggdroot/indentLine', requires = {'lukas-reineke/indent-blankline.nvim'}},
-  {'takac/vim-hardtime'},
-  {'eraserhd/parinfer-rust', run = 'cargo build --release'},
-  {'vigoux/architext.nvim'},
-  {'tjdevries/express_line.nvim',
+  {"Yggdroot/indentLine", requires = {"lukas-reineke/indent-blankline.nvim"}},
+  {"takac/vim-hardtime"},
+  {"eraserhd/parinfer-rust", run = "cargo build --release"},
+  {"vigoux/architext.nvim"},
+  {
+    "tjdevries/express_line.nvim",
     requires = {
-      'nvim-lua/plenary.nvim',
-      'kyazdani42/nvim-web-devicons'
+      "nvim-lua/plenary.nvim",
+      "kyazdani42/nvim-web-devicons"
     },
-    config = 'require [[expressline_setup]]'
+    config = "require [[expressline_setup]]"
   },
-  {'antoinemadec/FixCursorHold.nvim'},
-  {'voldikss/vim-floaterm'},
-  {'nvim-lua/telescope.nvim',
+  {"antoinemadec/FixCursorHold.nvim"},
+  {"voldikss/vim-floaterm"},
+  {
+    "nvim-lua/telescope.nvim",
     requires = {
-      'nvim-lua/popup.nvim',
-      'nvim-lua/plenary.nvim'
+      "nvim-lua/popup.nvim",
+      "nvim-lua/plenary.nvim"
     }
   },
-  {'mhartington/formatter.nvim',
+  {
+    "mhartington/formatter.nvim",
     config = {
       [[
         require('format').setup({
@@ -95,10 +105,9 @@ local packages = {
       ]]
     }
   },
-  {'nvim-treesitter/completion-treesitter'},
-  {'ThePrimeagen/vim-be-good'},
-  {'npxbr/glow.nvim', run = ':GlowInstall', ft = 'markdown'}
+  {"nvim-treesitter/completion-treesitter"},
+  {"ThePrimeagen/vim-be-good"},
+  {"npxbr/glow.nvim", run = ":GlowInstall", ft = "markdown"}
 }
-
 
 return packages
