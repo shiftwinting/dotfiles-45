87e4b1087d7547e81c7e821ddf01451c2986d464
Chinmay Dalal <chinmay.dalal.22012001@gmail.com>
2020-11-28 23:47:54 +0530

    Add scripts

---
+++ scripts/.local/bin/swaynag.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+swaynag -t warning -m 'Hello. Do you really want to exit sway? This will end your Wayland session.' \
+                                                 -b '🌚️Suspend' 'systemctl suspend' \
+                                                 -b '⚡️Shutdown' 'systemctl -i poweroff' \
+                                                 -b '🌀️Reboot' 'systemctl -i reboot' \
+                                                 -b '🚪️Yes, exit sway' 'swaymsg exit' \
+						 -b 'Lock' 'swaylock' \
+                                                 --button-background=ff9100 --button-border-size=3px --border=fcc99d --text=092e47
