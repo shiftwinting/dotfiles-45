a647f04a813ba3045204f41e9c445d200fb25ac1
Chinmay Dalal <chinmay.dalal.22012001@gmail.com>
2020-12-19 20:28:18 +0530

    update

--- scripts/.local/bin/swaynag.sh
+++ scripts/.local/bin/swaynag.sh
@@ -2,13 +2,16 @@
 swaynag -t warning -m 'Hello. Do you really want to exit sway? This will end your Wayland session.' \
                                                  -b '🌚️ Suspend' 'systemctl suspend' \
                                                  -b '⚡️ Shutdown' 'shutdown now' \
-                                                 -b '🌀️ Reboot' 'systemctl -i reboot' \
-                                                 -b '🚪️ Yes, exit sway' 'swaymsg exit' \
-                                                 -b ' Lock' 'swaylock' \
+                                                 -b '🔁 Reboot' 'systemctl -i reboot' \
+                                                 -b '🚪 Yes, exit sway' 'swaymsg exit' \
+                                                 -b '🔒 Lock' 'swaylock' \
+                                                 -s '❌' \
                                                  --button-background=1c1b19 \
                                                  --button-border-size=0px \
-                                                 --border=FBB829 --text=D0BFA1 \
+                                                 --border=FBB829 \
+                                                 --text=D0BFA1 \
                                                  --background=1c1b19 \
                                                  --button-padding=5px \
                                                  --button-margin-right=10px \
-                                                 --message-padding=5px
+                                                 --message-padding=5px \
+                                                 --font="Fira Sans 10"
