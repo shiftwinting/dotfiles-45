a1537bef2ef6f54ad358d2f3f0f589bb4d8a9b8d
Chinmay Dalal <chinmay.dalal.22012001@gmail.com>
2020-12-21 10:59:32 +0530

    Dismiss swaynag after clicking a button

--- scripts/.local/bin/swaynag.sh
+++ scripts/.local/bin/swaynag.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 swaynag -t warning -m 'Hello. Do you really want to exit sway? This will end your Wayland session.' \
-                                                 -b '🌚️ Suspend' 'systemctl suspend' \
-                                                 -b '⚡️ Shutdown' 'shutdown now' \
-                                                 -b '🔁 Reboot' 'systemctl -i reboot' \
-                                                 -b '🚪 Yes, exit sway' 'swaymsg exit' \
-                                                 -b '🔒 Lock' 'swaylock' \
+                                                 -z '🌚️ Suspend' 'systemctl suspend' \
+                                                 -z '⚡️ Shutdown' 'shutdown now' \
+                                                 -z '🔁 Reboot' 'systemctl -i reboot' \
+                                                 -z '🚪 Yes, exit sway' 'swaymsg exit' \
+                                                 -z '🔒 Lock' 'swaylock' \
                                                  -s '❌' \
                                                  --button-background=1c1b19 \
                                                  --button-border-size=0px \
