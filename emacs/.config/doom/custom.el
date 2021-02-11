0d6577bc1cee7fc1607aa5b99a53dcf068b43543
Chinmay Dalal <chinmay.dalal.22012001@gmail.com>
2021-02-11 23:57:21 +0530

    Add doom :D

---
+++ emacs/.config/doom/custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(evil-mu4e tree-siiter tree-sitter-langs lsp-mode org-mime oj auth-source-xoauth2 cider all-the-icons adoc-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
